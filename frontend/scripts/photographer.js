2c089af3ba96f853664322e5e7558b6da5f1e7eb
Anil <kanilreddy867@gmail.com>
2023-05-08 00:10:15 +0530

    individual photographer page is now dynamically working

--- frontend/scripts/photographer.js
+++ frontend/scripts/photographer.js
@@ -7,3 +7,23 @@ const swiper = new Swiper('.swiper', {
     // }
     slidesPerView: 2
 });
+
+const photographer = localStorage.getItem("photographerId");
+const photographerName = document.getElementById("name")
+const photographerPlace = document.getElementById("place")
+const photographerCamera  = document.getElementById("camera")
+const photographerPrice = document.getElementById("price")
+const photographerExpertise = document.getElementById("expertise")
+const titleName = document.querySelector("title")
+
+fetch(`http://localhost:3000/user/${photographer}`)
+.then((res) => res.json())
+.then((data) => {
+    console.log(data.user)
+    photographerName.textContent = data.user.name
+    photographerPlace.textContent = data.user.address
+    photographerCamera.textContent = data.user.camera
+    photographerPrice.textContent = data.user.price
+    photographerExpertise.textContent = data.user.expertise
+    titleName.textContent += " " + data.user.name
+})
